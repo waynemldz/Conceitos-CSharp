ac76388ea050171b46d0ab5379b8ca3ef009def3
waynemldz <waynegabriel13@gmail.com>
2026-07-01 22:19:16 -0300

    feat: adiciona cadastro e listagem de livros em console

--- 01-HelloWorld/Program.cs
+++ 01-HelloWorld/Program.cs
@@ -9,25 +9,83 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Seja bem vindo ao App! Para começar, cadastre um livro.");
+        List<Livro> livros = new List<Livro>();
 
-        Livro novoLivro = new Livro();
+        bool rodando = true;
 
-        Console.WriteLine("Digite o título do livro que deseja cadastrar:");
-        novoLivro.Titulo = Console.ReadLine();
+        static void Menu()
+        {
+
+            Console.Clear();
+            Console.WriteLine("Seja bem vindo ao WayBooks!");
+
+            Console.WriteLine("Selecione o que você deseja:");
+            Console.WriteLine("1 - Cadastrar livro");
+            Console.WriteLine("2 - Listar livros");
+            Console.WriteLine("3 - Sair");
 
 
-        Console.WriteLine("Agora, digite o nome do autor:");
-        novoLivro.Autor = Console.ReadLine();
+            Console.Write("Digite a opção desejada: ");
+        }
+
+        while (rodando)
+        {
+
+            Menu();
+
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.Clear();
+
+                    Livro novoLivro = new Livro();
+
+                    Console.WriteLine("Digite o título do livro que deseja cadastrar:");
+                    novoLivro.Titulo = Console.ReadLine();
 
 
-        Console.WriteLine("Pra finalizar, digite o ano do livro:");
-        novoLivro.Ano = int.Parse(Console.ReadLine()); 
+                    Console.WriteLine("Agora, digite o nome do autor:");
+                    novoLivro.Autor = Console.ReadLine();
+
+
+                    Console.WriteLine("Pra finalizar, digite o ano do livro:");
+                    novoLivro.Ano = int.Parse(Console.ReadLine());
+
+                    livros.Add(novoLivro);
+
+                    Console.Clear();
+
+                    Console.WriteLine("Livro cadastrado! \n");
+                    Console.WriteLine($"Título: {novoLivro.Titulo}");
+                    Console.WriteLine($"Autor: {novoLivro.Autor}");
+                    Console.WriteLine($"Ano: {novoLivro.Ano}");
+                    Console.WriteLine("\n Pressione Enter para voltar ao menu principal ou Esc para fechar o app");
+
+                    ConsoleKeyInfo tecla = Console.ReadKey();
+
+                    if (tecla.Key == ConsoleKey.Escape)
+                    {
+                        rodando = false;
+                    }
+
+                    break;
+
+                case 2:
+                    Console.Clear();
+                    foreach (var livro in livros)
+                    {
+                        Console.WriteLine($"{livro.Titulo} - {livro.Autor} ({livro.Ano})");
+                    }
+                    Console.WriteLine("\nPressione qualquer tecla para voltar...");
+                    Console.ReadKey();
+                    break;
+
+                case 3:
+                    rodando = false;
+                    break;
+            }
+        }
         
-
-        Console.WriteLine("Livro cadastrado!");
-        Console.WriteLine($"Título: {novoLivro.Titulo}");
-        Console.WriteLine($"Autor: {novoLivro.Autor}");
-        Console.WriteLine($"Ano: {novoLivro.Ano}");
     }
 }
